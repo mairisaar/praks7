1eeea3b518a2d01cb88e9d699c6ac8e7ab69e068
mairisaar <mairi.saar@khk.ee>
2018-10-10 11:34:58 +0300

    Lisatud grammide kalkuleerimise võimalus.

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 //DOM Elements
 let kgInput = document.getElementById('kgInput');
+let gramOutput = document.getElementById('gramOutput');
 
 //Listening to events
 //e = event
 kgInput.addEventListener('input', e => {
     let kg = e.target.value;
-    console.log(kg);
+    
+    gramOutput.innerHTML = `${kg * 1000} g`; //grammid on kg korda 1000, lausena kuvatakse ühikud ka
 });
+
