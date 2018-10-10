15d8dbe7bd2c630754263842d1c154479b74e7a9
mairisaar <mairi.saar@khk.ee>
2018-10-10 12:41:18 +0300

    Lisatud tabeli kuvamise tingimused.

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ let kgInput = document.getElementById('kgInput');
 let gramOutput = document.getElementById('gramOutput');
 let poundOutput = document.getElementById('poundOutput');
 let ozOutput = document.getElementById('ozOutput');
+let output = document.getElementById('output');
+
+output.style.visibility = 'hidden';
 
 //Listening to events
 //e = event
 kgInput.addEventListener('input', e => {
     let kg = e.target.value;
+    
+    if (kg === '0'){
+        output.style.visibility = 'hidden';
+    } else {
+    output.style.visibility = 'visible';
+    };
     
     gramOutput.innerHTML = `${kg * 1000} g`; //grammid on kg korda 1000, lauses kuvatakse ühikud 
     
